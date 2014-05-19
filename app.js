5c70047846712c99b368448ff8c1a8331d7b6d94
Huevy <huevy@gmail.com>
2014-05-19 21:37:59 +0300

    fix

--- app.js
+++ app.js
@@ -14,11 +14,18 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/', function(req, res) {
+  res.send('<h1>It works!</h1>');
+});
+
+
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res) {
+  res.send('Not found', 404);
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  // next(err);
 });
 
 /// error handlers
@@ -44,6 +51,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
